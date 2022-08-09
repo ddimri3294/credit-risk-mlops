007df6613b6bff33677005bb53f0b4b4deb8f5ff
Divyanshu Dimri <divyanshu.dimri@jai-kisan.com>
2022-08-09 16:36:04 +0530

    commit all changes

---
+++ train.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import json
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=100)
+    parser.add_argument('--learning-rate', type=float, default=0.1)
+
+    # an alternative way to load hyperparameters via SM_HPS environment variable.
+    parser.add_argument('--sm-hps', type=json.loads, default=os.environ['SM_HPS'])
+
+    # input data and model directories
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--test', type=str, default=os.environ['SM_CHANNEL_TESTING'])
+
+    args = parser.parse_args()
+
+    training_data_path = os.path.join(args.train, "housing.csv")
+    testing_data_path = os.path.join(args.test, "housing.csv")
+
+    # using it as variable
+    model_dir = os.environ['SM_MODEL_DIR']
+
+    print(training_data_path)
+    print(testing_data_path)
+    print(model_dir)
